437144bd135dd55b60893a29078e098527b5e539
Asharudeen Ali KT <131525899+achu-BSL@users.noreply.github.com>
2023-05-22 21:43:40 +0530

    Add files via upload

---
+++ server/middleware/auth.js
@@ -0,0 +1,22 @@
+// import { Jwt } from "jsonwebtoken"
+import ENV from '../config.js'
+
+/**auth middleware */
+export default async function Auth(req, res, next){
+    try {
+
+        //access authrize header to validate request
+        const token = req.header.authorization.split(" ")[1]
+
+
+        //retrive the user details for the logged in user
+        const decodedToken = await Jwt.verify(token, ENV.JWT_SECRET)
+
+        req.user = decodedToken
+        
+        res.json(decodedToken)
+
+    } catch (err) {
+        res.status(401).send({err: "Authentication Failed..!"})
+    }
+}
